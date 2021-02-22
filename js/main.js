86fba724881b5a186f3c97ded8853363bb3ea7f6
aoikubo <sakubo8440@gmail.com>
2021-02-23 01:42:48 +0900

    add svg

--- js/main.js
+++ js/main.js
@@ -1,4 +1,20 @@
 $(function() {
+  $(window).scroll(function () {
+    let scrollValue = $(window).scrollTop();
+    let wh = $(window).height();
+    $(".slide-trigger").each(function (idx, elm) {
+      let tp = $(elm).offset().top - wh / 2;
+      if (scrollValue > tp) {
+        $(elm).find(".slide").addClass("in");
+      }
+    });
+  });
+
+  $("#logo").click(function () {
+    $(".first-box").addClass("active");
+    $(".second-box").toggleClass("active");
+  });
+  
 
   
 
